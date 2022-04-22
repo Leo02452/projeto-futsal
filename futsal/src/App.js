3d5ddd9ce84c19c265e3c2045f0179122bb2de03
Leo Araujo <leonardo_02452@hotmail.com>
2022-04-22 15:29:27 -0300

    feat: add routes and basic pages

--- futsal/src/App.js
+++ futsal/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import MatchDay from './components/MatchDay';
+import Financial from './pages/Financial';
+import History from './pages/History';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Match from './pages/Match';
+import Settings from './pages/Settings';
+import Stats from './pages/Stats';
+import { Route, Switch } from 'react-router-dom';
 
 export default function App() {
     return (
       <main>
-        <MatchDay />
+        <Switch>
+          <Route path="/financial" component={ Financial } />
+          <Route path="/history" component={ History } />
+          <Route path="/home" component={ Home } />
+          <Route path="/match" component={ Match } />
+          <Route path="/settings" component={ Settings } />
+          <Route path="/stats" component={ Stats } />
+          <Route path="/" component={ Login } exact />
+        </Switch>
       </main>
     );
 }
