4a063bbb61b807f45eec58b6c2e41fec0fa8fc0a
Leo Araujo <leonardo_02452@hotmail.com>
2022-07-02 11:52:41 -0300

    feat: componentes com options além de jogadores
    Criação dos componentes permite personalização das opções de cada evento

---
+++ src/components/FoulsOptions.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import MatchDayContext from '../contexts/MatchDayContext';
+
+function FoulsOptions(props) {
+  const { onInputChange } = props;
+  const { event } = useContext(MatchDayContext);  
+
+  return (
+    <>
+      <label htmlFor="unknown" className="player-input">
+        Não sei
+        <input
+          type="radio"
+          id="unknown"
+          name={ `${event.type}Player` }
+          value="Desconhecido"
+          onChange={ onInputChange }
+        />
+      </label>
+      { event.team === 'teamB' && (
+      <label htmlFor="hand-foul" className="player-input">
+        Mão
+        <input
+          type="radio"
+          id="hand-foul"
+          name={ `${event.type}Player` }
+          value="Mão"
+          onChange={ onInputChange }
+        />
+      </label>
+      ) }
+    </>
+  )
+}
+
+export default FoulsOptions;
